213d2b1b3f2ccff59bcff1f51336add2e5056bce
Alexander <sssadakov@yandex.com>
2025-12-21 01:01:01 +0300

    minor chages in plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,10 +13,9 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
   -- Цветовые схемы
-	{ "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
-
-	{ 'norcalli/nvim-colorizer.lua' },
+  { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true},
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { 'norcalli/nvim-colorizer.lua' },
 
 
   -- иконки
@@ -32,13 +31,13 @@ require("lazy").setup({
   },
 
   -- Информационная строка внизу
-	{
+  {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' }
   },
 
   -- Табы вверху
-	{'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
+  {'akinsho/bufferline.nvim', version = "*", dependencies = 'nvim-tree/nvim-web-devicons'},
 
   -- иконки в выпадающем списке автодополнений (прямо как в vscode)
   { 'onsails/lspkind-nvim' },
@@ -46,26 +45,25 @@ require("lazy").setup({
   -- 'folke/which-key.nvim',            -- Всплывающие подсказки по клавишам
 
   -- LSP
-	{ 'williamboman/mason.nvim' },
-	
+  { 'williamboman/mason.nvim' },
+  { "williamboman/mason-lspconfig.nvim" },
   { 'neovim/nvim-lspconfig', },
-	
   { 'nvim-treesitter/nvim-treesitter' }, -- Улучшенная подсветка синтаксиса
 
   -- Autocomplete
   { 'hrsh7th/nvim-cmp' },
-	{ 'hrsh7th/cmp-nvim-lsp' },
-	{ 'hrsh7th/cmp-buffer' },
-	{ 'hrsh7th/cmp-path' },
+  { 'hrsh7th/cmp-nvim-lsp' },
+  { 'hrsh7th/cmp-buffer' },
+  { 'hrsh7th/cmp-path' },
   { 'hrsh7th/cmp-cmdline' },
   { 'saadparwaiz1/cmp_luasnip' },       -- автодополнения для сниппетов
   { 'L3MON4D3/LuaSnip' },                 -- движок сниппетов
-  
+
   -- набор готовых сниппетов для всех языков
   { "rafamadriz/friendly-snippets" },
 
   -- Comments
-	{
+  {
     'numToStr/Comment.nvim',
     opts = {},
     lazy = false,
@@ -110,40 +108,39 @@ require("lazy").setup({
         --]]
     end
   },
-  
+
   -- Даже если включена русская раскладка vim команды будут работать
   { "powerman/vim-plugin-ruscmd" },
 
   -- Закрывает автоматом скобки
   { 'cohama/lexima.vim' },
-
-	{ 'RRethy/vim-illuminate' },
+  { 'RRethy/vim-illuminate' },
 
   -- Файловый менеджер
-	{
-	  "nvim-tree/nvim-tree.lua",
-	  version = "*",
-	  lazy = false,
-	  dependencies = {
-	    "nvim-tree/nvim-web-devicons",
-	  },
-	  config = function()
-	    require("nvim-tree").setup {}
-	  end,
-	},
-  
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = "*",
+    lazy = false,
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+      require("nvim-tree").setup {}
+    end,
+  },
+
   -- Навигация внутри файла по классам и функциям
   { 'majutsushi/tagbar' },
 
   -- Поиск
   {
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-		dependencies = { 'nvim-lua/plenary.nvim' }
-	},
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+  	dependencies = { 'nvim-lua/plenary.nvim' }
+  },
 
   -- Обрамляет или снимает обрамление. Выдели слово, нажми S и набери <h1>
   { 'tpope/vim-surround' },
-  
+
   -- 'Автоформатирование' кода для всех языков
   -- use 'vim-autoformat/vim-autoformat'
 
@@ -151,18 +148,17 @@ require("lazy").setup({
   {
     'dense-analysis/ale',
     config = function()
-        -- Configuration goes here.
-        local g = vim.g
-
-        g.ale_linters = {
-          python = {'pyright'},
-          lua = {'lua_language_server'},
-          c = {'clangd'},
-          cpp = {'clangd'}
-        }
+      -- Configuration goes here.
+      local g = vim.g
+      g.ale_linters = {
+        python = {'pyright'},
+        lua = {'lua_language_server'},
+        c = {'clangd'},
+        cpp = {'clangd'}
+      }
     end
   },
 
   -- Терминал
-	{'akinsho/toggleterm.nvim', version = "*", config = true},
+  {'akinsho/toggleterm.nvim', version = "*", config = true},
 })
