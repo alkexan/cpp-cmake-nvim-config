f8e686b3febc9ece8bea6c711fc0dc508a1994fe
Alexander <sssadakov@yandex.com>
2025-12-23 22:26:14 +0300

    настройка клавиш

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,81 +1,72 @@
-local map = vim.api.nvim_set_keymap
-local default_opts = {noremap = true, silent = true}
 local builtin = require('telescope.builtin')
 
-map('i', 'jk', '<esc>', default_opts)
+vim.keymap.set('i', 'jk', '<esc>')
 
 -- Системный буфер обмена ctrl+c
-map('v', '<C-c>', '"+y', default_opts)
-map('n', '<C-v>', '"+p', default_opts)
-map('i', '<C-v>', '<esc>"+p', default_opts)
+vim.keymap.set('v', '<C-c>', '"+y')
+vim.keymap.set('n', '<C-v>', '"+p')
+vim.keymap.set('i', '<C-v>', '<esc>"+p')
 
 -- Автоформат CTRL-f , как в нормальном, так и в insert режиме
-map('n', '<C-S-i>', ':Autoformat<CR>',  default_opts)
-map('i', '<C-S-i>', '<esc>:Autoformat<CR>', default_opts)
+vim.keymap.set('n', '<C-S-i>', ':Autoformat<CR>')
+vim.keymap.set('i', '<C-S-i>', '<esc>:Autoformat<CR>')
 
 -- Сохранение по CTRL-s , как в нормальном, так и в insert режиме
-map('v', '<C-s>', ':w<CR>',  default_opts)
-map('n', '<C-s>', ':w<CR>',  default_opts)
-map('i', '<C-s>', '<esc>:w<CR>', default_opts)
+vim.keymap.set('v', '<C-s>', ':w<CR>')
+vim.keymap.set('n', '<C-s>', ':w<CR>')
+vim.keymap.set('i', '<C-s>', '<esc>:w<CR>')
 
 -- Переключение вкладок с помощью shift-tab (akinsho/bufferline.nvim)
-map('n','<Tab>', ':BufferLineCycleNext<CR>', default_opts)
-map('n','<S-Tab>', ':BufferLineCyclePrev<CR>', default_opts)
+vim.keymap.set('n', 'L', ':BufferLineCycleNext<CR>', { desc = 'След. вкладка' })
+vim.keymap.set('n', 'H', ':BufferLineCyclePrev<CR>', { desc = 'Пред. вкладка' })
+
+-- Табуляция
+vim.keymap.set('v', '<Tab>', '>gv', { desc = 'Табуляция вправо' })
+vim.keymap.set('v', '<S-Tab>', '<gv', { desc = 'Табуляция влево' })
+vim.keymap.set('n', '<Tab>', '>>', { desc = 'Табуляция строки вправо' })
+vim.keymap.set('n', '<S-Tab>', '<<', { desc = 'Табуляция строки влево' })
 
 -- ctrl+b дерево файлов.
 -- Если Дерево открыто, то переместить фокус на него
 -- Иначе Открыть
-map('n', '<C-b>', ':NvimTreeRefresh<CR>:NvimTreeToggle<CR>', default_opts)
-map('i', '<C-b>', '<esc>:NvimTreeRefresh<CR>:NvimTreeToggle<CR>', default_opts)
-map('n', '<C-e>', ':NvimTreeOpen<CR>:NvimTreeFocus<CR>', default_opts)
-map('i', '<C-e>', '<esc>:NvimTreeOpen<CR>:NvimTreeFocus<CR>', default_opts)
+vim.keymap.set('n', '<C-b>', ':NvimTreeRefresh<CR>:NvimTreeToggle<CR>')
+vim.keymap.set('i', '<C-b>', '<esc>:NvimTreeRefresh<CR>:NvimTreeToggle<CR>')
+vim.keymap.set('n', '<C-e>', ':NvimTreeOpen<CR>:NvimTreeFocus<CR>')
+vim.keymap.set('i', '<C-e>', '<esc>:NvimTreeOpen<CR>:NvimTreeFocus<CR>')
 
 -- Выйти
-map('n', '<C-q>', ':q<CR>',  default_opts)
-map('i', '<C-q>', '<esc>:q<CR>', default_opts)
-map('v', '<C-q>', '<esc>:q<CR>', default_opts)
+vim.keymap.set('n', '<C-q>', ':q<CR>')
+vim.keymap.set('i', '<C-q>', '<esc>:q<CR>')
+vim.keymap.set('v', '<C-q>', '<esc>:q<CR>')
 
 -- клавиши для telescope
--- Поиск слова под курсором
-map('n', '<leader>fw', ':Telescope grep_string<CR>',  default_opts)
-map('v', '<leader>fw', ':Telescope grep_string<CR>',  default_opts)
-
--- Поиск слова в модальном окошке
-map('n', '<leader>fg', ':Telescope live_grep<CR>',  default_opts)
-map('v', '<leader>fg', ':Telescope live_grep<CR>',  default_opts)
-
--- Поиск файлов
-map('n', '<leader>ff', ':Telescope find_files<CR>',  default_opts)
-map('v', '<leader>ff', ':Telescope find_files<CR>',  default_opts)
-
--- Поиск в буфере
-map('n', '<leader>fb', ':Telescope buffers<CR>',  default_opts)
-map('v', '<leader>fb', ':Telescope buffers<CR>',  default_opts)
+vim.keymap.set('n', '<leader>fw', builtin.grep_string, {desc = 'Поиск слова под курсором'})
+vim.keymap.set('v', '<leader>fw', builtin.grep_string, {desc = 'Поиск слова под курсором'})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {desc = 'Поиск слова в модальном окошке'})
+vim.keymap.set('v', '<leader>fg', builtin.live_grep, {desc = 'Поиск слова в модальном окошке'})
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {desc = 'Поиск файлов'})
+vim.keymap.set('v', '<leader>ff', builtin.find_files, {desc = 'Поиск файлов'})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {desc = 'Поиск в буфере'})
+vim.keymap.set('v', '<leader>fb', builtin.buffers, {desc = 'Поиск в буфере'})
 
 -- Работа с Git
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>gg', builtin.git_commits, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {desc = 'Git branches'})
+vim.keymap.set('n', '<leader>gg', builtin.git_commits, {desc = 'Git commits'})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {desc = 'Git status'})
 
 -- Выбор цветовой схемы
-vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {})
-
--- Keymaps for DAP (Debugger)
-map('n', '<leader>db', ':DapToggleBreakpoint<CR>', default_opts)
-map('n', '<leader>dc', ':DapContinue<CR>', default_opts)
-map('n', '<leader>dn', ':DapStepOver<CR>', default_opts)
-map('n', '<leader>di', ':DapStepInto<CR>', default_opts)
+vim.keymap.set('n', '<leader>cs', builtin.colorscheme, {desc = 'Color scheme'})
 
 -----------------------------------------------------------
 -- Фн. клавиши по F1 .. F12
 -----------------------------------------------------------
 -- <F8>  Показ дерева классов и функций, плагин majutsushi/tagbar
-map('n', '<F9>', ':TagbarToggle<CR>', default_opts)
+vim.keymap.set('n', '<F9>', ':TagbarToggle<CR>')
 
 -- <F12> переключить комментарий в строке
--- map('n', '<F12>', '<gc>', {})
--- map('i', '<F12>', '<esc><gc>', {})
+-- vim.keymap.set('n', '<F12>', '<gc>', {})
+-- vim.keymap.set('i', '<F12>', '<esc><gc>', {})
 
 -- переключить блок комментария для визуального режима
--- map('v', '<F12>', '<gb>', default_opts)
+-- vim.keymap.set('v', '<F12>', '<gb>')
 
